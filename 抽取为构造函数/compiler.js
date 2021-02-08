8b16b7932a16ff94ece733212b87ec26e8f90984
Monday <axu1997@qq.com>
2021-02-08 14:42:23 +0800

    深度访问属性

--- 抽取为构造函数/compiler.js
+++ 抽取为构造函数/compiler.js
@@ -1,15 +1,27 @@
 let dBrace = /\{\{(.*?)\}\}/
 function compiler(el,data){
   // debugger
+  let getValuByPath = createGetValuePath(data)
   let childrens = el.childNodes
   for(let i = 0;i < childrens.length;i++){
     if(childrens[i].nodeType === 3){
       childrens[i].nodeValue = childrens[i].nodeValue.replace(dBrace,(_a,_b)=>{
-        return data[_b.trim()]
+        return getValuByPath(_b.trim())
       })
     }
     if(childrens[i].childNodes){
       compiler(childrens[i],data)
     }
+  }
+}
+// 深度获取路径的值(柯理化)
+function createGetValuePath(obj){
+  let res = obj
+  return function getValuByPath(path){
+    let paths = path.split('.')
+    while(prop = paths.shift()){
+      res = res[prop]
+    }
+    return res
   }
 }
